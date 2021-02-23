de777ced8fd9c8898c26db525ecd0c7ceba413f2
Sarim Aleem <sarimaleem99@gmail.com>
2021-02-23 13:10:26 -0600

    parse skeleton laid out (incomplete)

--- config.h
+++ config.h
@@ -1,6 +1,13 @@
 #ifndef MULTIACT_CONFIG
 #define MULTIACT_CONFIG
 
+#include <stdio.h>
+#include <stdbool.h>
+#include "action.h"
+
 void parse_config(char* fname);
+parent_action_t* build_parent(FILE* file);
+child_action_t* build_child(FILE* file);
+bool comment_line(char* line);
 
 #endif
