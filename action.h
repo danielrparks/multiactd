8cd87245b31749eebadc20aaa56f8cca56767233
Daniel Parks <danielrparks@gmail.com>
2021-02-28 14:32:40 -0600

    Fix undos

--- action.h
+++ action.h
@@ -12,13 +12,13 @@ struct child_action;
 typedef struct parent_action {
 	char* name;
 	char* command;
+	char* undo_command;
 	struct parent_action* value_next; // the next value in the hash table (collision)
 	struct child_action *children;
 	size_t num_children;
 	size_t count; // the number of times this action has happened within the time limit
 	size_t time_limit;
 	struct timespec time_next;
-	bool needs_wait;
 	bool in_queue;
 	pthread_mutex_t lock;
 } parent_action_t;
@@ -26,11 +26,8 @@ typedef struct parent_action {
 typedef struct child_action {
 	char* name;
 	char* command;
-	parent_action_t* parent;
-	size_t *undoes;
-	size_t num_undos;
+	char* undo_command;
 	size_t time_limit;
-	bool needs_wait;
 } child_action_t;
 
 #endif
